6701560512dc91f01ad9433b6f49426850c6ded8
Laith Harb <laithharb@Laiths-iMac.local>
2022-06-14 13:10:30 -0400

    commangs

--- 01-Dockerfile/node/index.js
+++ 01-Dockerfile/node/index.js
@@ -11,3 +11,21 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+// TO VIEW ALL IMAGES
+// docker images
+
+// TO REMOVE AN IMAGE
+// docker image rm <image-id>
+
+// TO BUILD AN IMAGE
+// docker build . -t nodeapp
+
+// RUN A CONTAINER OFF AN IMAGE
+// docker run --name my-node-app -d nodeapp
+
+// PORT MAPPING
+// docker run --name my-node-app -d -p 3000:3000 nodeapp
+
+// VIEW ALL CONTAINERS
+// docker ps
